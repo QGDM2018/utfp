06097cc3fa1623414c12051ebed0005ce99d8111
vvlj <1921749915@qq.com>
2019-12-04 12:47:18 +0800

    add saving

--- pre_process.py
+++ pre_process.py
@@ -1,5 +1,6 @@
 from datetime import timedelta
 import pickle
+# from tqdm import tqdm
 # 时间离散化，向下取整
 def tZoneMode(t,n):
     '''时间区间，向下取整
@@ -9,6 +10,25 @@ def tZoneMode(t,n):
     '''
     # 对分钟取整
     return t - timedelta(minutes=t.minute%n,seconds=t.second)
+
+def saving():
+    first = [100115, 100245, 100246, 100374, 100249, 100003, 100004, 100397, 100019, 100020, 100285, 100159, 100287,
+             100288, 100164, 100041, 100300, 100434, 100179, 100180, 100053, 100183, 100315, 100316, 100061, 100193,
+             100066, 100451, 100069, 100200, 100329, 100457, 100340, 100343, 100217]
+    mapping = {}
+    sPredRoad = set(pd.read_csv('data/testCrossroadFlow/submit_example.csv')['crossroadID'])
+    df = pd.read_csv('data/trainCrossroadFlow/roadnet.csv')
+    for arr in df.values:
+        h, t =arr[0],arr[1]
+        mapping[h] = mapping.get(h,[]) + [t]
+    predMapping = {}
+    for r in sPredRoad:
+        vs = mapping.get(r)
+        if vs is not None:
+            predMapping[r] = vs[0]
+        # else:
+        #     print(r)
+    return predMapping
 
 import pandas as pd
 import json
@@ -25,6 +45,10 @@ class PreProcessor:
             self.buffer = {}
             self.buffer['sCrossroadID'] = set(self.load_csv(1)['crossroadID'])
 
+        # self.lDfFlow = []
+        # for i in tqdm(range(1, 24)):  #
+        #     self.lDfFlow.append(self.load_csv(i))
+
 
     def __del__(self):
         # 保存缓存数据
@@ -38,7 +62,7 @@ class PreProcessor:
             dirpath = 'data/trainCrossroadFlow/train_trafficFlow_'
         else:
             dirpath = 'data/testCrossroadFlow/test_trafficFlow_'
-        return  pd.read_csv(dirpath+f'{i}.csv',encoding=encoding)   # 读取1号的
+        return  pd.read_csv(dirpath+f'{i}.csv',encoding=encoding)[['timestamp','crossroadID','vehicleID'] ]  # 读取1号的
 
     def load_flow(self,i):
         '''载入并处理流量数据'''
@@ -93,6 +117,7 @@ class PreProcessor:
             dfFlow,dFlow = self.load_flow(i)
         return dfFlow,dFlow
 
+
     def get_roadFlow_total(self,roadId):
         '''获取单个路口所有车流量数据
         :param roadId: 道路Id
@@ -127,4 +152,27 @@ class PreProcessor:
         except Exception as e:
             print(e)
             dfFlow,dFlow = self.load_timeFlow(i)
+        return dfFlow,dFlow
+
+    # 待测
+    def loadm_flow(self,i):
+        '''载入并处理流量数据'''
+        dfFlow =self.lDfFlow[i]
+        dFlow =  self.cal_flow(dfFlow)   # 计算流量
+        with open(f'data/tmp/flow_{i}','wb') as f:
+            pickle.dump(dFlow,f)
+        return dfFlow,dFlow
+    def getm_roadFlow(self,i) -> object:
+        '''获取单个路口各个时段车流量数据
+        :param i:天数
+        :return:
+            dfFlow：pd.DataFrame 原始车流数据表
+            dFlow: {crossroadID:pd.Series} 车流量时序数据
+        '''
+        try:
+            with open(f'data/tmp/flow_{i}','rb') as f:
+                dFlow = pickle.load(f)
+            dfFlow = self.lDfFlow[i]
+        except:
+            dfFlow,dFlow = self.load_flow(i)
         return dfFlow,dFlow
--- runex.py
+++ runex.py
@@ -1,22 +1,44 @@
-from pre_process import PreProcessor,pd
+from pre_process import PreProcessor,pd,saving
+from multiprocessing.pool import Pool
+from multiprocessing import freeze_support
 import matplotlib.pyplot as plt
 plt.rcParams['font.sans-serif'] = ['Simhei']
 plt.rcParams['axes.unicode_minus'] = False
 
-# ****载入数据
-day = 3
-prp = PreProcessor()    # 数据管理器
-# for i in range(1,24):
-#     dfFlow,dFlow =prp.get_roadFlow(i)	# 原始车流数据表，车流量时序数据 n
-# dfFlow,dFlow =prp.get_roadFlow(day)	# 原始车流数据表，车流量时序数据 n
-# dfFlow,dFlow =prp.get_timeFlow(day)	# 原始车流数据表，车流量时序数据 n
+def done():
+    freeze_support()
+    # ******载入数据******
+    day = 3
+    prp = PreProcessor()  # 数据管理器
+    sPredRoad, mapping = saving()
+    # for i in range(1,24):
+    #     dfFlow,dFlow =prp.get_roadFlow(i)	# 原始车流数据表，车流量时序数据 n
+    # dfFlow,dFlow =prp.get_roadFlow(day)	# 原始车流数据表，车流量时序数据 n
+    # dfFlow,dFlow =prp.get_timeFlow(day)	# 原始车流数据表，车流量时序数据 n
 
-roadFlow = prp.get_roadFlow_total(100001)
-# *****绘图示例
-# key = list(dFlow.keys())[0]
-# seFolw =dFlow[key]
-# seFolw.plot()
-# plt.title(f'{day}号交通口{key}车流量时序图')
-# plt.ylabel('车流量/5min')
-# plt.xlabel('时间/t')
-# plt.show()
+    # ******缓存数据******
+    # from os import walk
+    # visited = set()
+    # for _,_,files in walk(r'E:\数据挖掘项目\qgTask\utfp\data\tmp'):
+    #     for f in files:
+    #         if 'road' in f:
+    #             visited.add(f.split('_')[1])
+    # lRoadId = set(prp.load_csv(1)['crossroadID'])
+    # lRoadId ^= visited
+    # pool = Pool(4)
+    # pool.map(prp.get_roadFlow_total,lRoadId)
+
+    # roadFlow = prp.get_roadFlow_total(100001)
+    # *****绘图示例******
+    # key = list(dFlow.keys())[0]
+    # seFolw =dFlow[key]
+    # seFolw.plot()
+    # plt.title(f'{day}号交通口{key}车流量时序图')
+    # plt.ylabel('车流量/5min')
+    # plt.xlabel('时间/t')
+    # plt.show()
+
+if __name__ == '__main__':
+    day = 3
+    prp = PreProcessor()  # 数据管理器
+    predMapping = saving()
